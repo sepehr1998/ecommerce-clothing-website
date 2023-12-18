ff2023c591ca35587490233ec5c1cf943fdafdd7
Sepehr Samadi <samadisepehr@gmail.com>
2023-12-18 14:19:11 +0200

    user creation implemented

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
 
@@ -29,3 +30,26 @@ const firebaseConfig = {
 
    export const auth = getAuth();
    export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+   export const db = getFirestore();
+
+   export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return userDocRef;
+   }
