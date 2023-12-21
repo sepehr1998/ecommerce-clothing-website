1d2abea7a9b160c14166da80191d784b8421dab3
Sepehr Samadi <samadisepehr@gmail.com>
2023-12-21 13:31:35 +0200

    [1]: checkout page created [2]: add or remove quanitity implemented [3]: remove item from cart implemented [4]: total price calculation is done

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,9 +3,15 @@ import Button from '../button/button.component'
 import CartItem from '../cart-item/cart-item.component'
 import { useContext } from 'react'
 import { CartContext } from '../../contexts/cart-context'
+import { useNavigate } from 'react-router-dom'
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckoutHandle = () => {
+        navigate('/checkout');
+    }
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
@@ -13,7 +19,7 @@ const CartDropdown = () => {
                     <CartItem key={item.id} cartItem={item} />
                 ))}
             </div>
-            <Button>Go to checkout</Button>
+            <Button onClick={goToCheckoutHandle}>Go to checkout</Button>
         </div>
     )
 }
